2a1bffb30f639ca18cec965ac673c6726d0391f9
Wardhani Sukoco <wardhanisukoco@gmail.com>
2023-10-27 02:55:10 +0700

    fix: predictor

--- src/predictor.py
+++ src/predictor.py
@@ -30,12 +30,12 @@ class Predictor(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         with torch.no_grad():
             # 1. apply transforms
-            x  = # YOUR CODE HERE
+            x  = self.transforms(x)
             # 2. get the logits
-            x  = # YOUR CODE HERE
+            x  = self.model(x)
             # 3. apply softmax
             #    HINT: remmeber to apply softmax across dim=1
-            x  = # YOUR CODE HERE
+            x  =  F.softmax(x, dim=1)
 
             return x
 
